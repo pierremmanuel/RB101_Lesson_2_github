b8288f6bf6ebfbd322c981e4dd1e58b649c3d273
Pierre-Emmanuel Roussel <pierre.emmanuel.rssl@gmail.com>
2021-03-01 20:21:25 +0100

    Finished the calculator.rb file/exercise, added the the messages in a config file named calculator_messages.yml

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,25 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'fr'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts ("=> #{message}")
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
 end
 
 def operation_to_message(op)
@@ -17,38 +37,39 @@ def operation_to_message(op)
   end
 end
 
-prompt("Welcome to calculator! Enter your name")
 name = nil
+prompt(messages('welcome', LANGUAGE))
 loop {
   name = Kernel.gets().chomp()
   if name.empty?
-    prompt("Make sure to write your name!")
+    prompt(messages('valid_name', LANGUAGE))
   else
     break
   end
 }
 
-prompt("Hi #{name}!")
+puts "=> " + messages('hello', LANGUAGE) + name +"!"
 
 num1 = nil
 num2 = nil
 loop {
   loop {
-    prompt("Please enter two different numbers...")
+    prompt(messages('number_entry', LANGUAGE))
     num1 = Kernel.gets().chomp()
     num2 = Kernel.gets().chomp()
-    break if num1.to_i.to_s == num1 && num2.to_i.to_s == num2
-    prompt("You have to use valid numbers")
+    break if number?(num1) && number?(num2)
+    prompt(messages('wrong_numbers', LANGUAGE))
   }
 
-  operator_prompt = <<-blabla
-    What operation would you like to perform #{name}?
-    1) add
-    2) substract
-    3) multiply
-    4) divide
-  blabla
-  prompt(operator_prompt)
+  # operator_prompt = <<-MSG
+  #   What operation would you like to perform?
+  #   1) add
+  #   2) substract
+  #   3) multiply
+  #   4) divide
+  # MSG
+
+  prompt(messages('select_operation', LANGUAGE))
 
   operator = nil
   loop {
@@ -56,32 +77,35 @@ loop {
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("You have to pick a number between 1 and 4")
+      prompt(messages('wrong_operation', LANGUAGE))
     end
   }
 
-  prompt("#{operation_to_message(operator)} the two numbers")
-
   case operator
     when "1"
-    prompt("The result of #{num1} + #{num2} is #{num1.to_i + num2.to_i}")
+      prompt(messages('one', LANGUAGE))
+      prompt(messages('result', LANGUAGE))
+      print "#{num1.to_i + num2.to_i}"
     when "2"
-    prompt("The result of #{num1} - #{num2} is #{num1.to_i - num2.to_i}")
+      prompt(messages('two', LANGUAGE))
+      prompt(messages('result', LANGUAGE))
+      print "#{num1.to_i - num2.to_i}"
     when "3"
-    prompt("The result of #{num1} * #{num2} is #{num1.to_i * num2.to_i}")
+      prompt(messages('three', LANGUAGE))
+      prompt(messages('result', LANGUAGE))
+      print "#{num1.to_i * num2.to_i}"
     when "4"
+      prompt(messages('four', LANGUAGE))
       if num2.to_i.zero?
-        prompt("You cannot divide by 0, do it again")
+        prompt(messages('zero_division', LANGUAGE))
         next
       else
-        prompt("The result of #{num1} / #{num2} is #{num1.to_f / num2.to_f}")
+        prompt(messages('result', LANGUAGE))
+        puts "#{num1.to_i / num2.to_i}"
       end
-    else
-    prompt("You did not pick a valid operator, do it again")
-    next
   end
 
-  prompt("Do you want to do another operation? Yes / No")
-  answer = Kernel.gets().chomp().downcase
-  break if answer == "no"
+  prompt(messages('continue', LANGUAGE))
+  answer = gets.chomp.downcase
+  break if answer == "no" || answer == "non"
 }
